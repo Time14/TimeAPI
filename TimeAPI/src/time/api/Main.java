f2985bd533b9968c46c43f4ec7b7e2beebd84037
SporreKing <alfred@familjensporre.se>
2015-09-10 15:18:14 +0200

    Added GameStateManager Constructor

--- TimeAPI/src/time/api/Main.java
+++ TimeAPI/src/time/api/Main.java
@@ -1,12 +1,9 @@
 package time.api;
 
-import time.api.gamestate.GameStateManager;
-import time.api.library.Library;
-
 public class Main {
 	
 	public static final void main(String[] args) {
 		
-		new Game().run("TimeWars", 800, 600);
+		new Game().run("TimeWars", 1280, 720);
 	}
 }
